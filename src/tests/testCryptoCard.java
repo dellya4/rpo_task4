b04388709821374c174ab9c0e339792a632bccd4
Асель Абдрахманова <dellya4>
2026-03-17 21:26:23 +0500

    fix: fixing test

--- src/tests/testCryptoCard.java
+++ src/tests/testCryptoCard.java
@@ -1,10 +1,12 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
 import payment.CryptoPayment;
 import payment.PaymentProcessor;
 
 public class testCryptoCard {
 
+    @Test
     public void testCryptoCardPayment() {
         PaymentProcessor processor = new PaymentProcessor();
         processor.setStrategy(new CryptoPayment());
